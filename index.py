23ac78bd56436add5d2e39bee01b09fe7321f40d
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-02-27 22:56:31 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,57 @@
+# import datetime
+# x=datetime.datetime.now()
+
+# for date and time with miliseconds 
+# print(x)
+
+# for only year
+# print(x.year)
+
+# for only month
+# print(x.month)
+
+# for full dayname
+# print(x.strftime("%A")) 
+
+# for short dayname
+# print(x.strftime("%a"))
+
+# for  minutes
+# print(x.strftime("%M"))
+
+# for  seconds
+# print(x.strftime("%S")) 
+
+
+# for no.of month
+# print(x.strftime("%m"))
+
+# for short year
+# print(x.strftime("%y"))
+
+# for full year
+# print(x.strftime("%Y"))
+
+# for 24 hour format
+# print(x.strftime("%H"))
+
+# for 12 hour fromat
+# print(x.strftime("%I"))
+
+# for AM PM
+# print(x.strftime("%p"))
+
+# for full date and time
+# print(x.strftime("%c"))
+
+# for print % symbol
+# print(x.strftime("%%"))
+
+# for date representation
+# print(x.strftime("%x"))
+ 
+# for date representation
+# print(x.strftime("%X"))
+
+# for date,year,weekday
+# print(x.strftime("%m-%y:%A")
